60989d5aee5cf2a5734ac91e8fda064c50391425
Gabriel Dutra <nesk.frz@gmail.com>
2017-04-16 23:34:37 -0300

    Alterado tipo de música para mp3

--- catapults.c
+++ catapults.c
@@ -281,7 +281,7 @@ void inicializa(void)
     if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 ){
         printf( "SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError() );
     }
-    somBackground = Mix_LoadMUS( "soundfx/somBackground.wav" );
+    somBackground = Mix_LoadMUS( "soundfx/somBackground.mp3" );
     if( somBackground == NULL ){
         printf( "Failed to load medium sound effect! SDL_mixer Error: %s\n", Mix_GetError() );
     }
